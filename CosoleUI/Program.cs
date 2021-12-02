557d1d9f1faa2e0ace6da74dacd1c7a6c0bb394e
umitakbas82 <umit.akbas@hotmail.com.tr>
2021-12-02 10:02:03 +0300

    MyFinalProject

---
+++ CosoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CosoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
