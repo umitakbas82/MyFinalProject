8bb268e4ff6c9af47dec2553c0b9d7815d867e62
umitakbas82 <umit.akbas@hotmail.com.tr>
2022-01-08 03:05:27 +0300

    Utilities and results adding in Core

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
@@ -20,6 +21,13 @@ namespace Business.Concrete
             _productDal = iProductDal;
         }
 
+        public IResult Add(Product product)
+        {
+            //business codes
+            _productDal.Add(product);
+            return new Result(true,"Product Added");
+        }
+
         public List<Product> GetAll()
         {
             //İs Kodları 
@@ -30,6 +38,11 @@ namespace Business.Concrete
         public List<Product> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
