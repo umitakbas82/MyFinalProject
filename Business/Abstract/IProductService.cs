2baf4c3447b917036af7da295e03260db5ff5124
umitakbas82 <umit.akbas@hotmail.com.tr>
2022-01-20 22:54:04 +0300

    Add IDataResult Interface

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -11,12 +11,12 @@ namespace Business.Abstract
 {
     public interface IProductService
     {
-        List<Product> GetAll();
-        List<Product> GetAllByCategoryId(int Id);
-        List<Product> GetByUnitPrice(decimal min, decimal max);
+        IDataResult<List<Product>> GetAll();        
+        IDataResult <List<Product>> GetAllByCategoryId(int Id);
+        IDataResult <List<Product>> GetByUnitPrice(decimal min, decimal max);
 
-        List<ProductDetailDto> GetProductDetails();
-        Product GetById(int productId);
+        IDataResult <List<ProductDetailDto>> GetProductDetails();
+        IDataResult<Product> GetById(int productId);
 
         IResult Add(Product product);
 
